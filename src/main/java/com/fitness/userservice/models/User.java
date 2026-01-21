b2824af4f3fb6a03f325a407cecf45b579ce5a2b
Rukshesh Talwar <rukshesh.talwar@sleepcountry.ca>
2026-01-21 20:11:40 +0530

    Initial Commit

---
+++ src/main/java/com/fitness/userservice/models/User.java
@@ -0,0 +1,33 @@
+package com.fitness.userservice.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    private String id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+}
